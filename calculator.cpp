16eb3c975f139164c81478bcfd1aa228cc18cadc
Your Name <you@example.com>
2019-12-14 19:11:05 +0000

    adding two numbers

---
+++ calculator.cpp
@@ -0,0 +1,11 @@
+#include<iostream.h>
+using namespace std;
+int main()
+{
+  int firstnumber, secondnumber, sumoftwonumbers;
+  cout<<"enter two integers:";
+  cin>>first number>> second number;
+  sumoftwonumbers=firstnumber+secondnumber;
+  cout<<firstnumber<<"+"<<secondnumber<<"="<<sumoftwonumbers;
+  return 0;
+}
