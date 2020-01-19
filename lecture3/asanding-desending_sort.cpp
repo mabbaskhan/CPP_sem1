906ba0f4438da5f05ff9cdbde3360589753418b0
Your Name <you@example.com>
2020-01-19 15:18:52 +0000

    Program on asending and desending sort

---
+++ lecture3/asanding-desending_sort.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+int a, b, c, d, e;
+cout<<"Enter five integers: \n";
+cin>>a>>b>>c>>d>>e;
+cout<<"The numbers in asending order are: \n ";
+cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e;
+cout<<endl;
+cout<<"The number in desending order: \n";
+cout<<e<<" "<<d<<" "<<c<<" "<<b<<" "<<a;
+cout<<endl;
+return 0;
+}
