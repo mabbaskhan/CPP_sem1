eae1106148a269bd61fd90e97f672361e0efc139
ishtiaq2 <ishtiaq2@kth.se>
2020-03-14 18:50:13 +0100

    commented poly

--- CPP_sem2/inheritance/ObjTest.cpp
+++ CPP_sem2/inheritance/ObjTest.cpp
@@ -7,21 +7,24 @@ class ObjTest {
     int a = 10;
     
     ObjTest() {
-
+        name = "";
     }
     
     ObjTest(string name) {
         this->name = name;
     }
 
+    // Poly morphism, function overloading: 
+    // two are more same functions with same name and return type
+    //but different parameter list
     void printObj() {
         cout<<name << endl;
         cout << a << endl;
         int b = 20;
     }
 
-    void printObj2() {
-        cout<< a;
+    void printObj(string arg) {
+        cout<< arg;
     }
 };
 
@@ -31,5 +34,5 @@ int main() {
     obj.printObj();
     obj.a = 20;
     obj.printObj();
-    obj.printObj2();
+    obj.printObj("my args");
 }
